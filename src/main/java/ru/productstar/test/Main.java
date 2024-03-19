86d05a3a3fec8edd4dafee396447429d92901f60
kopytow <kopytow@mail.ru>
2024-03-19 21:36:42 +0700

    Простое работающее приложение.

---
+++ src/main/java/ru/productstar/test/Main.java
@@ -0,0 +1,14 @@
+package ru.productstar.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by User 19.03.2024 21:12
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/test/java/ru/productstar/test/MainTest.java
@@ -0,0 +1,16 @@
+package ru.productstar.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by User 19.03.2024 21:18
+ */
+@SpringBootTest
+public class MainTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
